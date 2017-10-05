99136cede9096fced9c99c8d8bf337d1d2e8f7cc
Sarah Zajac <sarahmzajac@gmail.com>
2017-10-05 14:26:38 -0400

    made tests work

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,32 +1,32 @@
 import reducer from "./reducer";
-import { createTodo, toggleTodo, filterTodos } from "./actions";
+// import { createTodo, toggleTodo, filterTodos } from "./actions";
+// import * as actions from "./actions";
+import * as types from './actions';
+import * as actions from './actions';
 
 
 describe("CREATE_TODO", function () {
     test("creates a new todo", function () {
         const initialState = {todos: [], nextId: 1}
-        const state = reducer(initialState, createTodo("Test"));
+        const state = reducer(initialState, actions.createTodo("Test"));
         expect(state.todos).toHaveLength(1);
         expect(state.todos[0]).toEqual({id: 1, done: false, text: "Test"});
     })
-
-    test("updates nextId", function () {
-        const initialState = {todos: [], nextId: 1}
-        const state = reducer(initialState, createTodo("Test"));
-        expect(state.nextId).toEqual(2);
-    })
 })
 
-// describe("TOGGLE_TODO", function () {
-//     test("toggles todos", function () {
-//         const initialState = {todos: [] done: false}
-//         const state = reducer(initialState, toggleTodo("Test"));
-//         expect(state.todos).toEqual({done: true});
-//     })
-// })
-//
-// test('it decrements counter on button click', () => {
-//     const component = mount(<App />);
-//     component.find(".btn-decrement").simulate('click');
-//     expect(component.state('score')).toEqual(-1);
-//   })
+
+    describe("TOGGLE_TODO", function () {
+      test('toggle a todo', function(){
+        const initialState = {todos: [{id: 1, done: false, text: "Test"}]};
+        const state = reducer(initialState, actions.toggleTodo(1));
+        expect(state.todos[0]).toEqual({id: 1, done: true, text: "Test"});
+      })
+    });
+
+    describe('FILTER_TODOS', () => {
+      test('filters todo based on filterType', () => {
+        const initialState = {todos: [{id: 1, done: true, text: 'Test'}], nextId: 2, filter: 'all'};
+        const state = reducer(initialState, actions.filterTodos('completed'));
+        expect(state.filter).toEqual('completed')
+      })
+    })
